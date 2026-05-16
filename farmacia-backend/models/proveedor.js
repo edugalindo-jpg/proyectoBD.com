a8198619a04ae21503501cfdde1e6ade201cf4d1
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 09:00:29 -0500

    Create proveedor.js

---
+++ farmacia-backend/models/proveedor.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const ProveedorSchema = new mongoose.Schema({
+  nombre: String,
+  ruc: String,
+  telefono: String,
+  direccion: String
+});
+
+
+module.exports = mongoose.model("Proveedor", ProveedorSchema);
