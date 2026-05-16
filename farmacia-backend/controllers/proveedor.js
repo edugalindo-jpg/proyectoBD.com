d8afbb504924bc00defbf07859c49c0902be09ce
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 09:00:37 -0500

    Create proveedor.js

---
+++ farmacia-backend/controllers/proveedor.js
@@ -0,0 +1,36 @@
+const Proveedor = require("../models/proveedor");
+
+exports.crear = async (req, res) => {
+
+  try {
+
+    const data = await Proveedor.create(req.body);
+
+    res.json(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+};
+
+exports.listar = async (req, res) => {
+
+  try {
+
+    const data = await Proveedor.find();
+
+    res.json(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
+};
