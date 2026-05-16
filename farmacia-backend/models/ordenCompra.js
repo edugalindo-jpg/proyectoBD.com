1a3baf2ce131b53820a24eff52a3be9cebc11f32
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 09:00:32 -0500

    Create ordenCompra.js

---
+++ farmacia-backend/models/ordenCompra.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const OrdenCompraSchema = new mongoose.Schema({
+  empleado: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Empleado",
+    required: true
+  },
+  proveedor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Proveedor",
+    required: true
+  },
+
+  productos: [
+    {
+      producto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Producto"
+      },
+      cantidad: Number,
+      precioUnitario: Number,
+      precioTotal: Number
+    }
+  ],
+
+  sumaTotal: Number,
+
+  fecha: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = mongoose.model("OrdenCompra", OrdenCompraSchema);
