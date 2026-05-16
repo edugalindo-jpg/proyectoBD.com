ca84bc91bcd0296f1fd60cc626336f1d7d49a785
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 09:00:55 -0500

    Create db.js

---
+++ farmacia-backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/farmacia");
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = connectDB;
