ac38040aa2e92c13e0080f8659470afe58fb924a
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 09:00:18 -0500

    Create producto.js

---
+++ farmacia-backend/routes/producto.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+
+const Producto = require("../models/producto");
+
+router.post("/", async (req, res) => {
+
+  const data = await Producto.create(req.body);
+
+  res.json(data);
+
+});
+
+router.get("/", async (req, res) => {
+
+  const data = await Producto.find();
+
+  res.json(data);
+
+});
+
+router.get("/:nombre", async (req, res) => {
+
+  try {
+
+    const producto = await Producto.findOne({
+      nombre: req.params.nombre
+    });
+
+    if (!producto) {
+
+      return res.status(404).json({
+        mensaje: "Producto no encontrado"
+      });
+
+    }
+
+    res.json(producto);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
+});
+
+
+module.exports = router;
