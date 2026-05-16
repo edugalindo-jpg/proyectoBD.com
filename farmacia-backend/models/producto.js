da43f896d28d04f2bcca764a2eb8530f39ae3a77
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 09:00:26 -0500

    Create producto.js

---
+++ farmacia-backend/models/producto.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ProductoSchema = new mongoose.Schema({
+  nombre: String,
+  precio: Number,
+  stock: Number,
+  proveedor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Proveedor"
+  }
+});
+
+
+module.exports = mongoose.model("Producto", ProductoSchema);
