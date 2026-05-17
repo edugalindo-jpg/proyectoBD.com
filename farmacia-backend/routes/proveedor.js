125f633f9a954cb43354de3eaf2329425e7977b7
edugalindo-jpg <edu.galindo@tecsup.edu.pe>
2026-05-16 21:16:39 -0500

    Update proveedor.js

--- farmacia-backend/routes/proveedor.js
+++ farmacia-backend/routes/proveedor.js
@@ -6,7 +6,6 @@ router.post("/", async (req, res) => {
   const data = await Proveedor.create(req.body);
   res.json(data);
 });
-
 router.get("/", async (req, res) => {
   const data = await Proveedor.find();
   res.json(data);
